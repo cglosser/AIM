92e5ecfb274e26130cd92a6dae90863988530f5d
Connor Glosser <glosser1@gmail.com>
2015-02-23 18:11:51 -0500

    Plot lambda matrix

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import AIM
 import numpy as np
+from matplotlib.pylab import * 
+
+#circ = AIM.sample_unit_circle(144)
+#grid  = AIM.Grid(8)
+#basis = AIM.build_basis_set(AIM.shift_to_first_quadrant(circ))
+
+def main():
+    circ = AIM.sample_unit_circle(128)
+    grid = AIM.Grid(8)
+    basis = AIM.build_basis_set(AIM.shift_to_first_quadrant(circ))
+
+    m = AIM.construct_lambda(grid, basis)
+
+    matshow(m.toarray())
+    show()
 
 if __name__ == "__main__":
     main()
-
-grid  = AIM.Grid(4)
-basis = AIM.build_basis_set(AIM.sample_unit_circle(32))
-
-def main():
-    pass
-
